90661831099e588cd80c0514a0b0f3fb1df09a69
Ylis Riisalu <ylis@totu.ee>
2018-03-24 13:45:41 +0200

    first

--- DelTimeSizeBased/Program.cs
+++ DelTimeSizeBased/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,46 @@ namespace DelTimeSizeBased
     {
         static void Main(string[] args)
         {
+            // Garbage collection
+            bool Dryrun = true;
+            foreach(string s in args)
+            {
+                if (s == "DEL")
+                {
+                    Dryrun = false;
+                }
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //getfile list
+            if (Directory.Exists(Settings.Default.dir))
+            {
+                var dirinfo = new DirectoryInfo(Settings.Default.dir);
+                if (Dryrun)
+                {
+                    Console.WriteLine(" DRY RUN");
+                }
+                else
+                {
+                    Console.WriteLine(" WORKING RUN");
+                }
+                int countdown = Settings.Default.BigCount;
+                string status = "[ OK  ]";
+                foreach (FileInfo f in dirinfo.GetFiles().OrderByDescending(t => t.LastWriteTime))
+                {
+                    if (countdown <= 0) { status = "[ DEL ]"; }
+                    if (f.Length > Settings.Default.BigBytes) { countdown--; }
+                    
+
+                    Console.WriteLine(status+f.LastWriteTime.ToString() + " " + f.Length.ToString()+ " " + f.Name);
+                }
+
+            }
+            else { Console.WriteLine(string.Format(" Cant find Directory \"{0}\" ", Settings.Default.dir)); }
+
+
+            Console.ReadKey();
         }
     }
 }
--- DelTimeSizeBased/Properties/AssemblyInfo.cs
+++ DelTimeSizeBased/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DelTimeSizeBased")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Riisalu UÜ")]
 [assembly: AssemblyProduct("DelTimeSizeBased")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ad17830d-cf4a-459e-be06-def0489c719c")]
